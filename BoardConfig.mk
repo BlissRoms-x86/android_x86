92c9214813e249af5ec6af12c8dd7b39ae1b39d9
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-11-05 11:59:20 +0800

    android_x86: lollipop-x86 porting
    Based on android-5.0.0_r2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,31 +4,12 @@
 
 # The generic product target doesn't have any hardware-specific pieces.
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
 
 TARGET_ARCH := x86
 TARGET_CPU_ABI := x86
 TARGET_CPU_SMP := true
 
-SMALLER_FONT_FOOTPRINT := true
-MINIMAL_FONT_FOOTPRINT := true
-# Some framework code requires this to enable BT
-BOARD_HAVE_BLUETOOTH := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/generic/common/bluetooth
-
-# Build OpenGLES emulation libraries
-BUILD_EMULATOR_OPENGL := true
-BUILD_EMULATOR_OPENGL_DRIVER := true
-USE_OPENGL_RENDERER := true
-
-BOARD_USE_LEGACY_UI := true
-
-# share the same one across all mini-emulators
-BOARD_EGL_CFG := device/generic/goldfish/opengl/system/egl/egl.cfg
-
-# PDK does not use ext4 image, but it is added here to prevent build break.
 TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 576716800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 419430400
 BOARD_CACHEIMAGE_PARTITION_SIZE := 69206016
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
@@ -52,3 +33,5 @@ BOARD_SEPOLICY_UNION += \
         surfaceflinger.te \
         system_server.te \
         zygote.te
+
+include device/generic/common/BoardConfig.mk
--- android_x86.mk
+++ android_x86.mk
@@ -14,5 +14,11 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/android_x86.mk
+# includes the base of Android-x86 platform
+$(call inherit-product,device/generic/common/x86.mk)
+
+# Overrides
+PRODUCT_NAME := android_x86
+PRODUCT_BRAND := Android-x86
+PRODUCT_DEVICE := x86
+PRODUCT_MODEL := Generic Android-x86
